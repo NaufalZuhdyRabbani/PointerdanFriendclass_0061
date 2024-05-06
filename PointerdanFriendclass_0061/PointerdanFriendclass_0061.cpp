3ad50c64f80e4218cd92d039b64191d5b5d9caf3
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2024-05-06 15:34:58 +0700

    end

--- PointerdanFriendclass_0061/PointerdanFriendclass_0061.cpp
+++ PointerdanFriendclass_0061/PointerdanFriendclass_0061.cpp
@@ -112,3 +112,25 @@ int main()
 	setNama(joko, "Kairi Kumar");
 	return 0;
 } 
+
+class pelajar;
+class manusia
+{
+public:
+	void showNilaiPelajar(pelajar &x);
+};
+
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x)
+{
+	std::cout << x.nilai;
+}
